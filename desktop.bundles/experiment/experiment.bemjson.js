0538a8806fdc4bb7c06216492dd144b505a13e6c
Yury <kotovyuriy27@gmail.com>
2019-06-30 13:36:21 +0300

    added custom className blue_button, added custom experiment button to markup

---
+++ desktop.bundles/experiment/experiment.bemjson.js
@@ -0,0 +1,36 @@
+({
+    block : 'page',
+    title : 'experiment',
+    head : [
+        { elem : 'css', url : 'experiment.min.css' }
+    ],
+    scripts : [{ elem : 'js', url : 'experiment.min.js' }],
+    mods : { theme : 'islands' },
+    content : [
+        {
+            block : 'experiment',
+                content : [
+                {
+                    elem : 'greeting',
+                    content : 'Hello %user%!'
+                },
+                                {
+                    block : 'input',
+                    mods : {theme : 'islands', size : 'm'},
+                    name : 'name',
+                    placeholder : 'User name'
+                },
+                {
+                    block : 'button',
+                    mods : {theme : 'islands', size : 'm', type : 'submit'},
+                    text : 'Click'
+                },
+                                {
+                    block : 'button',
+                    mods : {blue: true, type : 'submit'},
+                    text : 'experiment button'
+                }
+            ]
+        }
+    ]
+})
